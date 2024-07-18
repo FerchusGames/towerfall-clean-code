ca0890ed3e4b473eb255718d9875f10d0bf4c50a
Ivan Herrera <ivan.herrera.m@outlook.com>
2024-07-17 18:35:38 -0600

    Added empty Managers and Databases

--- Towerfall/Assets/_Main/Scripts/Zenject/MainInstaller.cs
+++ Towerfall/Assets/_Main/Scripts/Zenject/MainInstaller.cs
@@ -1,9 +1,22 @@
+using Towerfall.Databases;
+using Towerfall.Managers;
 using Zenject;
 
-public class MainInstaller : MonoInstaller
+namespace Towerfall
 {
-    public override void InstallBindings()
+    public class MainInstaller : MonoInstaller
     {
-        
+        public override void InstallBindings()
+        {
+            // DATABASES
+            Container.BindInterfacesTo<OptionsDatabase>().AsSingle();
+            Container.BindInterfacesTo<PlayerProgressionDatabase>().AsSingle();
+            Container.BindInterfacesTo<StatsDatabase>().AsSingle();
+            
+            //MANAGERS
+            Container.BindInterfacesTo<PlayerManager>().AsSingle();
+            Container.BindInterfacesTo<InputManager>().AsSingle();
+            Container.BindInterfacesTo<StageManager>().AsSingle();
+        }
     }
 }
